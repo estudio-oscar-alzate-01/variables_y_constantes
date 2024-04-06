73b795202cd57816b66c1ebd878336aa8125981c
oscaralzate <oscardavidalzate2003garcia@gmail.com>
2024-04-06 18:36:42 -0500

    ejercicio5

---
+++ ejercicio5.js
@@ -0,0 +1,4 @@
+let peso = prompt("por favor, ingrese su peso");
+let altura = prompt("por favor, ingrese su altura");
+let masacorporal = altura * altura / peso
+console.log("tu IMC es:" + masacorporal);
